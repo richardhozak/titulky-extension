53d11af1d634522a9717ac76fdfee271cd40d55d
zxey <zxey@users.noreply.github.com>
2016-11-24 17:35:29 +0100

    Add checks before replacing tablelogin and tablesearch

--- titulky_ext.js
+++ titulky_ext.js
@@ -10,42 +10,50 @@
 	var search_icon = browser.extension.getURL("search.svg")
 
 	var tablelogin = document.getElementById("tablelogin")
-	var loginform = tablelogin.parentNode
-	tablelogin.remove()
+	if (tablelogin) {
+		var loginform = tablelogin.parentNode
+		tablelogin.remove()
 
-	loginform.insertAdjacentHTML('afterbegin', `
-		<div id="tablelogin-enh">
-			<div id="tablelogin-enh-header">
-				<p>LOGIN</p>
+		loginform.insertAdjacentHTML('afterbegin', `
+			<div id="tablelogin-enh">
+				<div id="tablelogin-enh-header">
+					<p>LOGIN</p>
+				</div>
+				<span id="tablelogin-enh-login"><input placeholder="Jméno" type="text" name="Login"></span>
+				<span id="tablelogin-enh-password"><input placeholder="Heslo" type="password" name="Password"></span>
+				<div id="tablelogin-enh-bottom">
+					<span id="remember-me-enh">
+						<label>
+							<input type="checkbox" name="foreverlog" value="1"> 
+							<span>Přihlásit trvale</span>
+						</label>
+					</span>
+					<span id="submit-enh">
+						<input type="submit" name="prihlasit" value="Přihlásit se">
+					</span>
+				</div>
 			</div>
-			<span id="tablelogin-enh-login"><input placeholder="Jméno" type="text" name="Login"></span>
-			<span id="tablelogin-enh-password"><input placeholder="Heslo" type="password" name="Password"></span>
-			<div id="tablelogin-enh-bottom">
-				<span id="remember-me-enh">
-					<label>
-						<input type="checkbox" name="foreverlog" value="1"> 
-						<span>Přihlásit trvale</span>
-					</label>
-				</span>
-				<span id="submit-enh">
-					<input type="submit" name="prihlasit" value="Přihlásit se">
-				</span>
-			</div>
-		</div>
-		`)
+			`)
+	}
+
 
 	var tablesearch = document.getElementById("tablesearch")
-	var tableform = tablesearch.parentNode
-	tablesearch.remove()
+	if (tablesearch) {
+		var tableform = tablesearch.parentNode
+		tablesearch.remove()
 
-	tableform.insertAdjacentHTML('afterbegin', `
-		<div id="tablesearch-enh">
-			<div id="tablesearch-enh-header">
-				<p>HLEDAT</p>
+		tableform.insertAdjacentHTML('afterbegin', `
+			<div id="tablesearch-enh">
+				<div id="tablesearch-enh-header">
+					<p>HLEDAT</p>
+				</div>
+				<div id="tablesearch-enh-search">
+					<input type="text" name="Fulltext" placeholder="Zadejte výraz">	
+				</div>
 			</div>
-			<div id="tablesearch-enh-search">
-				<input type="text" name="Fulltext" placeholder="Zadejte výraz">	
-				
+			`)
+	}
+
 			</div>
 		</div>
 		`)
